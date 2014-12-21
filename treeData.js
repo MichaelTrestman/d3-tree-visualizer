14f9cf18120b0c87dcbcff1ec6001514bcfa7247
Michael Trestman <michael.a.trestman@gmail.com>
2014-12-20 16:29:48 -0800

    adds new taxa

--- treeData.js
+++ treeData.js
@@ -263,7 +263,86 @@ var AnimalTree = {
   	  	  	  "species-diversity-source": "unknown",
   	  	  	  "taxon-age": "unknown",
   	  	  	  "taxon-age-source": "unknown",
-  	  	  	  "children": [],
+  	  	  	  "children": [
+                {
+                  "name": "Beetles (Coleoptera)",
+                  "species-diversity": 400000,
+                  "species-diversity-source": "http://en.wikipedia.org/wiki/Beetle#Overview",
+                  "taxon-age": 318,
+                  "taxon-age-source": "unknown",
+                  "children": [],
+                },
+                {
+                  "name": "Butterflies and Moths (Lepidoptera)",
+                  "species-diversity": 174000,
+                  "species-diversity-source": "http://en.wikipedia.org/wiki/Lepidoptera",
+                  "taxon-age": 190,
+                  "taxon-age-source": "unknown",
+                  "children": [],
+                },
+                {
+                  "name": "Flies (Diptera)",
+                  "species-diversity": 120000,
+                  "species-diversity-source": "http://en.wikipedia.org/wiki/Fly",
+                  "taxon-age": 240,
+                  "taxon-age-source": "unknown",
+                  "children": [],
+                },
+                {
+                  "name": "Hymenoptera",
+                  "species-diversity": 150000,
+                  "species-diversity-source": "http://en.wikipedia.org/wiki/Hymenoptera",
+                  "taxon-age": 251,
+                  "taxon-age-source": "unknown",
+                  "children": [
+                    {
+                      "name": "Symphyta",
+                      "species-diversity": "unknown",
+                      "species-diversity-source": "unknown",
+                      "taxon-age": "unknown",
+                      "taxon-age-source": "unknown",
+                      "children": [],
+                    },
+                    {
+                      "name": "Wasps and relatives (Apocrita)",
+                      "species-diversity": "unknown",
+                      "species-diversity-source": "unknown",
+                      "taxon-age": "unknown",
+                      "taxon-age-source": "unknown",
+                      "children": [
+                        {
+                          "name": "Bees (Anthophila)",
+                          "species-diversity": "unknown",
+                          "species-diversity-source": "unknown",
+                          "taxon-age": "unknown",
+                          "taxon-age-source": "unknown",
+                          "children": [],
+                        },
+                        {
+                          "name": "Ants (Formicidae)",
+                          "species-diversity": "unknown",
+                          "species-diversity-source": "unknown",
+                          "taxon-age": "unknown",
+                          "taxon-age-source": "unknown",
+                          "children": [],
+                        }
+
+
+                      ],
+                    }
+
+
+
+
+                  ],
+                }
+
+
+
+
+
+
+              ],
   	  	  	},
   	  	  	{
   	  	  	  "name": "Crustacea",
@@ -430,6 +509,15 @@ var AnimalTree = {
   	  	  	  	  	  	  "taxon-age-source": "unknown",
   	  	  	  	  	  	  "children": [],
   	  	  	  	  	  	},
+                        {
+                          "name": "Bats",
+                          "species-diversity": "unknown",
+                          "species-diversity-source": "unknown",
+                          "taxon-age": "unknown",
+                          "taxon-age-source": "unknown",
+                          "children": [],
+                        },
+
   	  	  	  	  	  	{
   	  	  	  	  	  	  "name": "Primates",
   	  	  	  	  	  	  "species-diversity": "unknown",
@@ -637,531 +725,5 @@ addCharge(AnimalTree, 1400	)
 
 
 
-
-var treeData =
-
-	{
-		'name': 'supertaxon of target taxon',
-		'children': [
-			{
-				'name': 'target taxon',
-				'children': [
-					{
-						'name': 'subtaxon 1 of target taxon'
-					},
-					{
-						'name': 'subtaxon 2 of target taxon'
-					},
-					{
-						'name': 'subtaxon 3 of target taxon'
-					},
-				]
-			},
-			{
-				'name': 'sister taxon 1 of target taxon'
-			},
-			{
-				'name': 'sister taxon 2 of target taxon'
-			},
-			{
-				'name': 'sister taxon 3 of target taxon'
-			}
-		]
-	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var otherthing = {'name': 'amniotes',
-'value': 100,
-'children': [
-
-	{'name': 'sauropsids',
-	'value': 100,
-	'children': [
-
-		{'name': 'diapsids',
-		'value': 100,
-		'children': [
-
-			{'name': 'petrolacosaurus',
-			'value': 100,
-			'children': []},
-
-			{'name': 'sauria',
-			'value': 100,
-			'children': [
-
-
-				{'name': 'lepidosauria',
-				'value': 100,
-				'children': [
-
-					{'name': 'lizards',
-					'value': 100,
-					'children': []},
-
-					{'name': 'snakes',
-					'value': 100,
-					'children': []},
-
-					{'name': 'turtles?',
-					'value': 100,
-					'children': []},
-
-				]},
-
-				{'name': 'archosauromorpha',
-				'value': 100,
-				'children': [
-
-					{'name': 'archosauriformes',
-					'value': 100,
-					'children': [
-
-						{'name': 'euparkeria',
-						'value': 100,
-						'children': []},
-
-						{'name': 'archosauria',
-						'value': 100,
-						'children': [
-
-
-							{'name': 'nil',
-							'value': 100,
-							'children': [
-								{'name': 'phytosaurs',
-								'value': 100,
-								'children': []},
-
-								{'name': 'nil',
-								'value': 100,
-								'children': [
-
-									{'name': 'crocodilians',
-									'value': 100,
-									'children': []},
-
-									{'name': 'rauisuchians',
-									'value': 100,
-									'children': []},
-									{'name': 'ornithosuchians',
-									'value': 100,
-									'children': []},
-									{'name': 'aetosaurs',
-									'value': 100,
-									'children': []},
-
-
-								]},
-
-							]},
-
-							{'name': 'ornithodirans',
-							'value': 100,
-							'children': [
-
-								{'name': 'pterosaurs',
-								'value': 100,
-								'children': []},
-
-								{'name': 'nil',
-								'value': 100,
-								'children': [
-
-									{'name': 'dinosauromorphs',
-									'value': 100,
-									'children': []},
-
-									{'name': 'dinosaurs',
-									'value': 100,
-									'children': [
-
-										{'name': 'ornithischians',
-										'value': 100,
-										'children': []},
-
-										{'name': 'nil',
-										'value': 100,
-										'children': [
-
-											{'name': 'sauropodomorphs',
-											'value': 100,
-											'children': []},
-
-											{'name': 'nil',
-											'value': 100,
-											'children': [
-
-												{'name': 'therapods',
-												'value': 100,
-												'children': []},
-
-												{'name': 'birds',
-												'value': 100,
-												'children': []},
-											]},
-										]},
-									]},
-								]},
-
-
-
-							]},
-
-
-							{'name': 'nil',
-							'value': 100,
-							'children': []},
-
-
-
-
-
-						]}
-
-
-
-
-
-					]},
-
-					{'name': 'euryapsida',
-					'value': 100,
-					'children': [
-
-						{'name': 'ichthyosauria',
-						'value': 100,
-						'children': []},
-
-						{'name': 'sauroptyrigia',
-						'value': 100,
-						'children': [
-
-							{'name': 'placodontia',
-							'value': 100,
-							'children': []},
-							{'name': 'pleisiosauria',
-							'value': 100,
-							'children': []}
-						]}
-					]},
-				]},
-
-
-			]}
-
-
-		]},
-
-	]},
-
-
-
-	{'name': 'synapsids... mammals',
-	'value': 100,
-	'children': [
-
-		{'name': 'carnivora',
-		'value': 100,
-		'children': [
-
-			{'name': 'canids',
-			'value': 100,
-			'children': [
-
-				{'name': 'dogs',
-				'value': 100,
-				'children': []},
-
-				{'name': 'foxes',
-				'value': 100,
-				'children': []}
-
-			]},
-
-		{'name': 'kitties',
-				'value': 100,
-				'children': [
-
-				{'name': 'lions',
-				'value': 100,
-				'children': []},
-
-				{'name': 'tigers',
-				'value': 100,
-				'children': []},
-
-				{'name': 'ocelots',
-				'value': 100,
-				'children': []},
-
-				{'name': 'housecats?',
-				'value': 100,
-				'children': []},
-
-				]},
-
-			{'name': 'pinnipeds',
-			'value': 100,
-			'children': [
-
-				{'name': 'sea lions',
-				'value': 100,
-				'children': []},
-
-				{'name': 'seals',
-				'value': 100,
-				'children': [
-
-					{'name': 'fur seal',
-					'value': 100,
-					'children': []},
-
-					{'name': 'cute little baby seal',
-					'value': 100,
-					'children': []},
-
-				]},
-
-			]}
-
-
-		]},
-
-		{'name': 'cetaceans',
-		'value': 100,
-		'children': [
-
-			{'name': 'odontocetes',
-			'value': 100,
-			'children': [
-
-				{'name': 'dolphins',
-					'value': 100,
-					'children': []},
-
-				{'name': 'orcas',
-				'value': 100,
-				'children': []},
-
-				{'name': 'pilot whales',
-					'value': 100,
-					'children': []},
-
-
-			]},
-
-
-			{'name': 'other whales?',
-			'value': 100,
-			'children': [
-
-				{'name': 'humpbacks',
-					'value': 100,
-					'children': []},
-
-				{'name': 'blue whales',
-				'value': 100,
-				'children': []},
-
-			]}
-
-
-		]},
-
-
-		{'name': 'bats!',
-				'value': 100,
-				'children': [
-
-				{'name': 'micro ones',
-				'value': 100,
-				'children': [
-
-					{'name': 'ugly bats',
-					'value': 100,
-					'children': []},
-
-					{'name': 'cute bats',
-					'value': 100,
-					'children': []},
-				]},
-
-				{'name': 'macro ones?',
-				'value': 100,
-				'children': [
-
-					{'name': 'flying foxes',
-					'value': 100,
-					'children': []},
-
-					{'name': 'probably fruit bats?',
-					'value': 100,
-					'children': [
-
-						{'name': 'apple bats',
-						'value': 100,
-						'children': []},
-
-						{'name': 'orange bats',
-						'value': 100,
-						'children': [
-
-							{'name': 'tangerine bats',
-						'value': 100,
-						'children': []},
-
-						{'name': 'blood orange bats',
-						'value': 100,
-						'children': [
-
-						{'name': 'type 0- blood orange bats',
-						'value': 100,
-						'children': []},
-
-						{'name': 'type AB+ blood orange bats',
-						'value': 100,
-						'children': []},
-
-						]},
-
-
-						]},
-
-						{'name': 'banana bats',
-						'value': 100,
-						'children': []},
-
-					]},
-
-				]},
-
-			]},
-
-
-		{'name': 'elephants',
-		'value': 100,
-		'children': [
-
-			{'name': 'regular elephants',
-				'value': 100,
-				'children': []},
-
-			{'name': 'mammoths',
-			'value': 100,
-			'children': [
-
-
-				{'name': 'woolly',
-				'value': 100,
-				'children': []},
-
-				{'name': 'silky',
-				'value': 100,
-				'children': []},
-
-			]},
-
-
-		]},
-
-
-
-		{'name': 'primates',
-		'value': 100,
-		'children': [
-
-			{'name': 'humans',
-			'value': 100,
-			'children': []},
-
-			{'name': 'chimps',
-			'value': 100,
-			'children': [
-
-				{'name': 'bonobos',
-				'value': 100,
-				'children': []},
-
-				{'name': 'pan troglodyes',
-				'value': 100,
-				'children': []}
-			]},
-
-			{'name': 'gorrillas',
-			'value': 100,
-			'children': []},
-
-			{'name': 'orangs',
-			'value': 100,
-			'children': []}
-
-
-		]},
-
-		{'name': 'ungulates',
-			'value': 100,
-			'children': [
-
-				{'name': 'llamas',
-				'value': 100,
-				'children': [
-
-					{'name': 'big llamas',
-					'value': 100,
-					'children': []},
-
-					{'name': 'little llamas',
-					'value': 100,
-					'children': []},
-				]},
-
-			{'name': 'unicorns',
-			'value': 100,
-			'children': []},
-
-			{'name': 'rhinos',
-			'value': 100,
-			'children': [
-				{'name': 'wooly rhinos',
-				'value': 100,
-				'children': []},
-				{'name': 'black rhinos',
-				'value': 100,
-				'children': []},
-				{'name': 'white rhinos',
-				'value': 100,
-				'children': []},
-			]},
-
-		]},
-
-
-	]}
-
-]}
 
 // exports.AnimalTree = AnimalTree;
