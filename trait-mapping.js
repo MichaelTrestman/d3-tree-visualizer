9f06f99db7e0f06afce1aca25c4ebebd4ae0f0fa
Michael Trestman <michael.a.trestman@gmail.com>
2014-12-20 16:30:31 -0800

    cleans up junk

--- trait-mapping.js
+++ trait-mapping.js
@@ -56,7 +56,3 @@ OriginHypotheses = {
 
 }
 
-
-DiscreteTraitDistribution.mapTraits({"conscious": 'dolphinately'}, OriginHypotheses.humans)
-
-// console.log( DiscreteTraitDistribution.outputTree() )
